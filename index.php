d00ffeb80b0b6c678ec429c8965c793a2098b73f
Martin Abbott <abbott01@googlemail.com>
2023-09-14 13:14:02 +0100

    installed composer and added slugify autoload

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php
+
+require __DIR__ . '/vendor/autoload.php';
+use Cocur\Slugify\Slugify;
+
 function greet($userName) {
     echo "Hello $userName! How are you today?";
 }
-greet("Martin")
+
+function greetSlug($userName) {
+    $slugified = new Slugify();
+    echo $slugified->slugify("Hello $userName! How are you today?");
+}
+
+greet("Martin");
+
+echo "\n";
+
+greetSlug("Martin");
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5e1eba7399de4d79d2c07f03d97c485baff777a2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5e1eba7399de4d79d2c07f03d97c485baff777a2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'cocur/slugify' => array(
+            'pretty_version' => 'v4.4.0',
+            'version' => '4.4.0.0',
+            'reference' => '4c6ed14a087ca061b220ffda640c07644946e2a0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cocur/slugify',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
